ed19383c2af2ee1593cb2cfea67b627a32982cdb
parkjuj <59850813+parkjuj@users.noreply.github.com>
2020-04-27 21:27:47 -0400

    E08b-Debugging
    Assignment changes.

--- src/UserInterface/UserInterface.gd
+++ src/UserInterface/UserInterface.gd
@@ -15,7 +15,6 @@ var paused: = false setget set_paused
 func _ready() -> void:
 	PlayerData.connect("updated", self, "update_interface")
 	PlayerData.connect("died", self, "_on_Player_died")
-	PlayerData.connect("reset", self, "_on_Player_reset")
 	update_interface()
 
 
@@ -24,17 +23,13 @@ func _on_Player_died() -> void:
 	title_label.text = MESSAGE_DIED
 
 
-func _on_Player_reset() -> void:
-	self.paused = false
-
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause") and title_label.text != MESSAGE_DIED:
 		self.paused = not self.paused
 
 
 func update_interface() -> void:
-	score_label.Text = "Score: %s" % PlayerData.score
+	score_label.text = "Score: %s" % PlayerData.score
 
 
 func set_paused(value: bool) -> void:
